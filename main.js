5f8aa55777cb234eb7bdb7f2f6ef1f3b84e5dc1b
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-26 13:38:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// let ucer ={
+//     lastname:"nurullayev",
+//     name:"miraziz",
+//     age:15,
+// };
+
+
+
+// ucer.username = 'miraziz1'
+// ucer.name ='mirazz'
+
+
+// // delete ucer.name
+// delete ucer.age
+
+// console.log(ucer.name);
+
+
+
+let ism = prompt('ism kiriting')
+let yosh = prompt('yosh kiriting')
+let maktab  = prompt('maktabingizni  kiriting')
+let familiya = prompt('familiya kiriting')
+let fan = prompt('yoqtirgan faningizni kiriting')
+let sinf = prompt('sinffongizni kiriting')
+
+let user = {
+    name:ism || miraziz,
+    age:yosh || 15,
+    maktab:maktab || 144,
+    familiya:familiya || nurullayev,
+    fan:fan || fan,
+    sinf:sinf || 9
+}
+
+console.log(user);
